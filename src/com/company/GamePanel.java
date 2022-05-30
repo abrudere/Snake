6330451d84444af904c07d87355902815a867830
Agnese Brudere <agnesebrudere@gmail.com>
2022-05-30 15:02:44 +0300

    Initial commit

---
+++ src/com/company/GamePanel.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class GamePanel extends JPanel implements ActionListener {
+
+    GamePanel() {
+
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
